bfb58fb1524028a97829656c7cb6157730a85302
AL Musheer <musheerayan@gmail.com>
2025-03-07 12:27:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,4 +35,5 @@ def remove_background():
     )
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000))) 
+    port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable
+    app.run(host='0.0.0.0', port=port) 
